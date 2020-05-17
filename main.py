e922c823177614d7c2719e2cebdf3b0542d4e50f
Vlad <you@example.com>
2020-05-17 17:05:56 +0300

    added ability to replace variables in expression

--- main.py
+++ main.py
@@ -20,46 +20,125 @@ class Display(metaclass=Singleton):
     def __init__(self):
         self.display(self.__template)
 
+    def __del__(self):
+        self.display('Bye!')
+
     @staticmethod
     def display(text: str):
         print(text)
 
     @staticmethod
-    def get_expt() -> str:
+    def get_expr() -> str:
         return input('-> ')
 
 
-class Store:
-    var_dict = {'h': 3, 'k': 6}
+class Store(metaclass=Singleton):
+    def __init__(self):
+        self.__var_dict = {}
+
+    def assign(self, expr: str):
+        identifier, value = expr.replace(' ', '').split('=')
+        if identifier.isalpha():
+            if value.isalpha():
+                value = self.get_var(value)
+            if value.isnumeric():
+                return self.__set_var(identifier, value)
+        return "SyntaxError. The identifier or value does not meet the requirements. \n" \
+               "To get more information use the <_/info> command."
+
+    def __set_var(self, key: str, value):
+        self.__var_dict[key] = value
+        return f'Variable: {key} created successfully.'
+
+    def get_var(self, key: str):
+        try:
+            return self.__var_dict[key]
+        except KeyError:
+            return f'This: {key} identifier does not exist.'
+
+    def del_var(self, key):
+        try:
+            self.__var_dict.pop(str(key))
+            return f'Variable: {key} deleted successfully.'
+        except KeyError:
+            return f'This: {key} identifier does not exist.'
+        except TypeError:
+            return "SyntaxError. Please review your command. \n" \
+                   "To get more information use the <_/info> command."
+
+    # def get_vars(self):
+    #     return self.__var_dict
 
 
 class Command(metaclass=Singleton):
     def __init__(self, store_obj: Store):
-        self.store_obj = store_obj
-        self.command_dict = {'/exit': lambda kwargs: exit(),
-                             '/help': lambda kwargs: self.__help(),
-                             '/del': lambda kwargs: self.__del_var(kwargs['var']),
-                             '/info': lambda kwargs: self.__info(kwargs)}
+        self.__store_obj = store_obj
+        self.__command_dict = {'_/exit': lambda kwargs: exit(),
+                               '_/help': lambda kwargs: self.__help(),
+                               '_/del': lambda kwargs: self.__del_var(*kwargs['var']),
+                               '_/info': lambda kwargs: self.__info(kwargs)}
 
     def __del_var(self, var: str) -> str:
-        try:
-            self.store_obj.var_dict.pop(var)
-            return 'text..'  # todo add text
-        except KeyError:
-            return 'error'  # todo add text
+        return self.__store_obj.del_var(var)
 
-    def __help(self):
+    def __help(self, kwargs):
         return 'text'  # todo add text
 
     def __info(self, kwargs):
         return 'info'  # todo action
 
     def process(self, command: str, **kwargs):
-        if command in self.command_dict:
-            return self.command_dict[command](kwargs)
+        if command in self.__command_dict:
+            return self.__command_dict[command](kwargs)
         else:
-            return 'text error'  # todo add text
+            return 'Unknown command.'
 
 
-class Calculator:
-    pass
+class Calculator(Store, Command):
+    def __init__(self):
+        super().__init__()
+        self.store = Store()
+        self.command = Command(self.store)
+
+    def _calculate(self, expr):
+        try:
+            expr = self.__check_expr(expr)
+            return eval(expr)  # todo check/contro eval
+        except SyntaxError:
+            return "SyntaxError. Please review your expression. \n" \
+                   "To get more information use the <_/info> command."
+
+    def __get_var_names(self, expr: str) -> list:
+        var_names = [char if char.isalpha() else ' ' for char in expr]
+        return ''.join(var_names).split()
+
+    def __replace_var(self, expr: str, variables: list):
+        for var in variables:
+            expr = expr.replace(var, self.store.get_var(var))
+        if variables[0] in expr:
+            raise SyntaxError
+        return expr
+
+    def __check_expr(self, expr):
+        variables = self.__get_var_names(expr)
+        if variables:
+            expr = self.__replace_var(expr, variables)
+        return expr
+
+    def perform_action(self, expr: str):
+        if expr.count('=') == 1 and len(expr.split()) == 3:
+            return self.store.assign(expr)
+        elif len(expr.split()) == 1 and expr.isalpha():
+            return self.store.get_var(expr)
+        elif '_/' in expr:
+            return self.command.process(expr.split()[0], var=expr.split()[1:])
+        else:
+            return self._calculate(expr)
+
+
+if __name__ == '__main__':
+    dis = Display()
+    calculator = Calculator()
+    while True:
+        data = dis.get_expr()
+        dis.display(calculator.perform_action(data))
